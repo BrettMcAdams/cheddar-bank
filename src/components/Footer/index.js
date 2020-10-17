d1a57fe17c5c4ebc1394047908b5051d469e22b3
unknown <brettmcadams96@gmail.com>
2020-10-17 18:37:54 -0400

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { animateScroll as scroll } from 'react-scroll';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to='/sign-up'>How it works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/'>Contact</FooterLink>
+              <FooterLink to='/'>Support</FooterLink>
+              <FooterLink to='/'>Destinations</FooterLink>
+              <FooterLink to='/'>Sponsorships</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to='/'>Submit Video</FooterLink>
+              <FooterLink to='/'>Ambassadors</FooterLink>
+              <FooterLink to='/'>Agency</FooterLink>
+              <FooterLink to='/'>Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to='/'>Instagram</FooterLink>
+              <FooterLink to='/'>Facebook</FooterLink>
+              <FooterLink to='/'>Youtube</FooterLink>
+              <FooterLink to='/'>Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/' onClick={toggleHome}>
+              Cheddar
+            </SocialLogo>
+            <WebsiteRights>Cheddar © 2020 All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href='//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+                target='_blank'
+                aria-label='Youtube'
+                rel='noopener noreferrer'
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                target='_blank'
+                aria-label='Twitter'
+                href='//www.twitter.com/briandesignz'
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
